bd79309a7b0a1e2f8bc65b2b1ed5d6fcf2744503
erpriyalchouksey <113887063+erpriyalchouksey@users.noreply.github.com>
2022-10-29 17:19:52 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListComponentComponent } from './list-component/list-component.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
